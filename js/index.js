1b2980c023158213d57c7ea67bc798865d640afe
Maribel Duran <maribelduranucm@gmail.com>
2017-03-19 22:04:13 -0700

    Created a controller and view object.

--- js/index.js
+++ js/index.js
@@ -35,3 +35,61 @@ var model = {
 		console.log("Current active timer is: ", this.activeTimer.name);
 	}
 };
+
+//controller
+//Once the timer has started, you can not update the SessionLength or BreakLength.
+var controller = {
+	createTimers: function(){
+		var sessionMinuteInput = document.getElementById("timer").textContent;
+		var sessionName = "New Task";
+		var breakMinuteInput = 5;
+		model.setSessionTimer(sessionMinuteInput, sessionName);
+		model.setBreakTimer(breakMinuteInput, "Break");
+		model.setActiveTimer(model.sessionTimer);
+	},
+	//Will use toggleTimer when I have a single button that changes between Start to Stop
+	toggleTimer: function(){
+	},
+	startTimer: function(){
+		if (!model.activeTimer.isRunning){
+			model.activeTimer.isRunning = true;
+			model.activeTimer.intervalID =  setInterval(function(){
+				//if seconds == 0, then we want to stop current timer and then start
+				//start the break session.
+				//var startBreakSession();
+				if (model.sessionTimer.seconds > 0){
+					model.sessionTimer.seconds --;
+					view.updateTimeLeft(model.sessionTimer.seconds);
+				}else{
+					//Reset current active timer
+					//stop current active timer
+					//make the non active timer, active and start running.
+					controller.stopTimer();
+				}	
+				}, 1000);
+		}
+	},
+	stopTimer: function(){
+		//can you resume an interval ID after it has been cleared?
+		clearInterval(model.activeTimer.intervalID);
+		model.activeTimer.isRunning = false;
+		console.log(model.activeTimer.intervalID);
+	},
+};
+
+var view = {
+	updateTimeLeft: function(seconds){
+		var time = this.secondsToMs(seconds);
+		var timeElement = document.getElementById("timer");
+		timeElement.innerHTML  = time;
+	},
+	secondsToMs: function(sec){
+			var m = Math.floor(sec / 60);
+			var s = sec % 60;
+			m = (m.toString().length == 1) ? "0" + m: m;
+			s = (s.toString().length == 1) ? "0" + s: s;
+			return (m + ":" + s);
+	}
+};
+
+controller.createTimers();
