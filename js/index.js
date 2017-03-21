bd0f755887eca3dae5cc38a7ae50f3238c1c353c
Maribel Duran <maribelduranucm@gmail.com>
2017-03-20 21:05:17 -0700

    controller.startTimer cis able to switch between a session timer and break timer when current active timer is finished.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
-//Using object literal to define a timer class
 //https://www.phpied.com/3-ways-to-define-a-javascript-class/
 //https://scotch.io/tutorials/better-javascript-with-es6-pt-ii-a-deep-dive-into-classes
+
+//Da7 77: Automatically switches between a Break and Session when time is up.
+//
 
 //timer class
 function timer(sec,name){
@@ -10,11 +12,13 @@ function timer(sec,name){
 	this.intervalID = "";
 	this.addMin = function (){
 		this.seconds += 60;
-		this.getTime();
 	};
 	this.reduceMin = function(){
 		this.seconds -= 60;
-		this.getTIme();
+		console.log(this.seconds);
+	},
+	this.resetTime = function(minutes){
+		this.seconds = minutes * 60;
 	}
 }
 //model
@@ -32,7 +36,6 @@ var model = {
 	},
 	setActiveTimer: function(timer){
 		this.activeTimer = timer;
-		console.log("Current active timer is: ", this.activeTimer.name);
 	}
 };
 
@@ -41,9 +44,8 @@ var model = {
 var controller = {
 	createTimers: function(){
 		var sessionMinuteInput = document.getElementById("timer").textContent;
-		var sessionName = "New Task";
 		var breakMinuteInput = 5;
-		model.setSessionTimer(sessionMinuteInput, sessionName);
+		model.setSessionTimer(sessionMinuteInput, "Session");
 		model.setBreakTimer(breakMinuteInput, "Break");
 		model.setActiveTimer(model.sessionTimer);
 	},
@@ -52,36 +54,62 @@ var controller = {
 	},
 	startTimer: function(){
 		if (!model.activeTimer.isRunning){
+			//want app to show initial time (i.e 25:00 should show first instead of 24:59);
+			view.displayTimeLeft(model.activeTimer.seconds);
+			view.displayTimerName(model.activeTimer.name);
 			model.activeTimer.isRunning = true;
 			model.activeTimer.intervalID =  setInterval(function(){
-				//if seconds == 0, then we want to stop current timer and then start
-				//start the break session.
-				//var startBreakSession();
-				if (model.sessionTimer.seconds > 0){
-					model.sessionTimer.seconds --;
-					view.updateTimeLeft(model.sessionTimer.seconds);
-				}else{
-					//Reset current active timer
-					//stop current active timer
-					//make the non active timer, active and start running.
-					controller.stopTimer();
+				if (model.activeTimer.seconds > 0){
+					model.activeTimer.seconds --;
+					view.displayTimeLeft(model.activeTimer.seconds);
+				}else{ //Reset and stop active timer. Then start the other timer.
+					if (model.activeTimer.name == "Session"){
+						var sessionLength = document.getElementById("sessionLength").textContent;
+						model.activeTimer.resetTime(sessionLength);
+						this.stopTimer();
+						model.activeTimer = model.breakTimer;
+						this.startTimer();
+					}
+					else if (model.activeTimer.name == "Break"){
+						var breakLength = document.getElementById("breakLength").textContent;
+						model.activeTimer.resetTime(breakLength);
+						this.stopTimer();
+						model.activeTimer = model.sessionTimer;
+						this.startTimer();
+					}
 				}	
-				}, 1000);
+				}.bind(this),1000);
 		}
 	},
 	stopTimer: function(){
-		//can you resume an interval ID after it has been cleared?
 		clearInterval(model.activeTimer.intervalID);
 		model.activeTimer.isRunning = false;
-		console.log(model.activeTimer.intervalID);
 	},
+	decSessionTime: function(){
+		if (!(model.sessionTimer.isRunning || model.breakTimer.isRunning) && model.sessionTimer.seconds > 60){
+			model.sessionTimer.reduceMin();
+			view.displaySessionTime(model.sessionTimer.seconds);
+		}
+	}
 };
 
 var view = {
-	updateTimeLeft: function(seconds){
+	displayTimeLeft: function(seconds){
 		var time = this.secondsToMs(seconds);
 		var timeElement = document.getElementById("timer");
 		timeElement.innerHTML  = time;
+	},
+	displayTimerName: function(name){
+		var timerName = document.getElementById("timerName");
+		timerName.innerHTML = name;
+
+	},
+	displaySessionTime: function(seconds){
+		var m = Math.floor(seconds/ 60);
+		document.getElementById("sessionLength").textContent = m;
+		document.getElementById("timer").textContent = m;
+
+
 	},
 	secondsToMs: function(sec){
 			var m = Math.floor(sec / 60);
