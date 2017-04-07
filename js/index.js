c5ddc298c5fd312c66ca65bccbc5d6fc8f42a91a
Maribel Duran <maribelduranucm@gmail.com>
2017-04-06 18:12:14 -0700

    Added Bootstrap modal wwhich holds settings options. This replaced the settingsButtons.

--- js/index.js
+++ js/index.js
@@ -55,9 +55,8 @@ var controller = {
 			model.breakTimer.resetTime(BREAK_TIME);
 			model.setActiveTimer(model.sessionTimer);
 			view.displayActiveTimer(model.activeTimer.name, model.activeTimer.seconds);
-			//view.displaySettings(25,5);
-			sesionSlider.setValue(SESSION_TIME, true);
-			breakSlider.setValue(BREAK_TIME, true);
+			view.updateSettings(SESSION_TIME, BREAK_TIME);
+			model.sessionsCompleted = 0;
 			view.updateSessionCounter(0);
 	},
 	startTimer: function(){
@@ -78,16 +77,16 @@ var controller = {
 	switchTimers: function(){ 
 			if (model.activeTimer.name == "Session"){
 				model.sessionsCompleted += 1;
-						view.updateSessionCounter(model.sessionsCompleted);
-						var sessionLength = document.getElementById("sessionLength").textContent;
-						model.activeTimer.resetTime(sessionLength);
-						this.stopTimer();
-						model.setActiveTimer(model.breakTimer);
+				view.updateSessionCounter(model.sessionsCompleted);
+				var sessionLength = document.getElementById("sessionLength").textContent;
+				model.activeTimer.resetTime(sessionLength);
+				this.stopTimer();
+				model.setActiveTimer(model.breakTimer);
 			}else if (model.activeTimer.name == "Break"){
-						var breakLength = document.getElementById("breakLength").textContent;
-						model.activeTimer.resetTime(breakLength);
-						this.stopTimer();
-						model.setActiveTimer(model.sessionTimer);
+				var breakLength = document.getElementById("breakLength").textContent;
+				model.activeTimer.resetTime(breakLength);
+				this.stopTimer();
+				model.setActiveTimer(model.sessionTimer);
 			}
 			this.startTimer();
 	},	
@@ -101,34 +100,21 @@ var controller = {
 		view.displaySessionLength(value);
 	},
 	updateBreakLength: function(value){
-			view.displayBreakLength(value);
-	},
-	toggleSettings: function(){
-		var settings = document.getElementById("settings");
-		if (settings.style.visibility === "visible"){
-			view.hideSettings();
-		}
-		else{
-			var sessionMinuteInput = document.getElementById("sessionLength").textContent;
-			var breakMinuteInput = document.getElementById("breakLength").textContent;
-			view.displaySettings(sessionMinuteInput,breakMinuteInput);
-		}
+		view.displayBreakLength(value);
 	},
 	undoSettingUpdates: function(){
+		//how can I save the original sessionLength
 		var currentSessionTime = view.secondsToMs(model.sessionTimer.seconds).split(":")[0];
 		var currentbreakTime = view.secondsToMs(model.breakTimer.seconds).split(":")[0];
-		sesionSlider.setValue(currentSessionTime, true);
-		breakSlider.setValue(currentbreakTime, true);
-		view.hideSettings();	
+		view.updateSettings(currentSessionTime, currentbreakTime);
 	},
 	updateSettings: function(){
 		this.stopTimer();
 		var sessionLength = document.getElementById("sessionLength").textContent;
-		var breakMinuteInput = document.getElementById("breakLength").textContent;
+		var breakLength = document.getElementById("breakLength").textContent;
 		model.sessionTimer.updateTime(sessionLength);
-		model.breakTimer.updateTime(breakMinuteInput);
+		model.breakTimer.updateTime(breakLength);
 		view.displayActiveTimer(model.activeTimer.name, model.activeTimer.seconds);
-		this.toggleSettings();
 	}
 };
 
@@ -140,12 +126,12 @@ var view = {
 	hideSettings: function(){
 		document.getElementById("settings").style.visibility = "hidden";
 	},
-	displaySettings: function(sessionLen, breakLen){
-		document.getElementById("sessionLength").textContent = sessionLen;
-		document.getElementById("breakLength").textContent = breakLen;
-		document.getElementById("settings").style.visibility = "visible";
+	updateSettings: function(sessionLen, breakLen){
+		sesionSlider.setValue(sessionLen, true);
+		breakSlider.setValue(breakLen, true);
 	},
 	displaySessionLength: function(minutes){
+		console.log("hello");
 		document.getElementById("sessionLength").textContent = minutes;
 	},
 	displayBreakLength: function(minutes){
@@ -169,11 +155,19 @@ var sesionSlider = new Slider("#sessionInput");
 sesionSlider.on("slide", function(sliderValue) {
 	controller.updateSessionLength(sliderValue);
 });
+sesionSlider.on("slideStop", function(sliderValue) {
+	controller.updateSessionLength(sliderValue);
+});
 
 var breakSlider = new Slider("#breakInput");
 breakSlider.on("slide", function(sliderValue) {
 	controller.updateBreakLength(sliderValue);
 });
+
+breakSlider.on("slideStop", function(sliderValue) {
+	controller.updateBreakLength(sliderValue);
+});
+
 //https://github.com/seiyria/bootstrap-slider
 //https://www.phpied.com/3-ways-to-define-a-javascript-class/
 //https://scotch.io/tutorials/better-javascript-with-es6-pt-ii-a-deep-dive-into-classes
